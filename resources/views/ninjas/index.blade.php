d12c754695cb07b80930c40d17b881b91cb1bc56
Romolo <romolotacmoyjr@gmail.com>
2025-03-28 10:51:11 +0800

    third commit

--- resources/views/ninjas/index.blade.php
+++ resources/views/ninjas/index.blade.php
@@ -1,21 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ninja Network | Home</title>
-</head>
-<body>
+<x-layout>
     <h2>Currently Available Ninjas</h2>
     <p>{{ $greeting }}</p>
 
     <ul>
-        @foreach($ninjas as $ninja)
+        @foreach ($ninjas as $ninja)
             <li>
-                <p>{{ $ninja['name'] }}</p>
-                <a href="/ninjas/{{ $ninja['id'] }}">View Details</a>
+                <x-card href="/ninjas/{{ $ninja['id'] }}" :highlight="$ninja['skill'] > 70">
+                    <h3>{{ $ninja['name'] }}</h3>
+                </x-card>
             </li>
         @endforeach
     </ul>
-</body>
-</html>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::get('/ninjas', function() {
     return view('ninjas.index', ['greeting' => 'hello world', 'ninjas' => $ninjas]);
 });
 
+Route::get('/ninjas/create', function() {
+    return view('ninjas.create');
+});
+
 Route::get('/ninjas/{id}', function($id) {
     return view('ninjas.show', ['id' => $id]);
 });
